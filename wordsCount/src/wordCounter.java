cf5301e9868b5fd1cb06eabd8b0acbed1f945512
gnl2021 <89617766+gnl2021@users.noreply.github.com>
2021-09-22 20:19:01 -0400

    last version

---
+++ wordsCount/src/wordCounter.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+public class wordCounter {
+
+
+    public static void main(String[] args) throws IOException {
+        menu myMenu= new menu();
+        myMenu.menu();
+    }
+}
